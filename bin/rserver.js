83ccfb587314d751e1d3659540988fe5653b3c98
ccckmit <ccckmit@gmail.com>
2016-03-28 12:49:09 +0800

    modify bin/rserver.js

--- bin/rserver.js
+++ bin/rserver.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var serveIndex = require('serve-index');
 var app = express();
-var dir = "../web";
-// var dir = path.join(__dirname, '/../web');
+//var dir = "../web";
+var dir = path.join(__dirname, '/../web');
 app.use('/', express.static(dir));
 // app.use('/', express.static(dir));
 app.use('/', serveIndex(dir, {'icons': true}));
@@ -12,4 +12,4 @@ app.use('/', serveIndex(dir, {'icons': true}));
 app.listen(3000);
 
 console.log('Server started: http://localhost:3000/');
-console.log('__dirname=', __dirname);
+console.log('__dirname=', __dirname, " dir=", dir);
